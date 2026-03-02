65b06ec3585969e6535b203c434e3aaeca637798
jaishree <jaishreesurage@gamil.com>
2026-03-02 10:53:18 +0530

    new changes

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
+from.import views
 
 urlpatterns = [
-    
+    path('', views.LogoPage, name='LogoPage'),
+    path('welcome/', views.welcome, name='welcome'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from.models import *
 
 # Create your views here.
+
+# logo view 
+def LogoPage(request):
+    return render(request,"app/logo.html")
+
+#welcome view 
+def welcome(request):
+    return render(request, "app/welcome.html")
+
