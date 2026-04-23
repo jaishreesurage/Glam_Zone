4ef1f55d29af65ba7f0b08ef6ce774550018917c
jaishree <jaishreesurage@gamil.com>
2026-04-23 20:14:12 +0530

    23-4-26

--- app/urls.py
+++ app/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('login/',views.login_success, name='login_success'),
     path('brands/', views.brands, name="brands"),
     path('products/<int:brand_id>/', views.products, name="products"),
-    path('cart/', views.cart, name="cart"),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart, name='cart'),
+    path('remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('buy-now/<int:product_id>/', views.buy_now, name='buy_now'),
+    path('checkout/', views.checkout, name='checkout'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,redirect
-from.models import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages
 from .models import Profile
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product,Brand
 # Create your views here.
 
 # logo view 
@@ -117,7 +117,58 @@ def products(request, brand_id):
     return render(request, "app/product.html", {"products": products})
 
 
-# Cart page
-def cart(request):
-    return render(request, "app/cart.html")
+# Add to Cart (sirf product add karega)
+def add_to_cart(request, product_id):
 
+    cart = request.session.get('cart', [])   # session se cart lo
+
+    cart.append(product_id)                  # product add karo
+
+    request.session['cart'] = cart           # save karo
+
+    return redirect('cart')                  # cart page par bhejo
+
+
+#  Cart Page (sirf show karega)
+def cart(request):
+
+    cart = request.session.get('cart', [])
+
+    products = Product.objects.filter(id__in=cart)
+
+    total = sum(product.price for product in products)
+
+    return render(request, "app/cart.html", {
+        'products': products,
+        'total': total
+    })
+
+def remove_from_cart(request, product_id):
+
+    cart = request.session.get('cart', [])
+
+    # अगर product cart में है → remove करो
+    if product_id in cart:
+        cart.remove(product_id)
+
+    request.session['cart'] = cart
+
+    return redirect('cart')
+
+# Buy Now
+def buy_now(request, product_id):
+
+    # single product store
+    request.session['buy_now'] = product_id
+
+    return redirect('checkout')
+
+def checkout(request):
+    product_id = request.session.get('buy_now')
+
+    product = None
+
+    if product_id:
+        product = get_object_or_404(Product, id=product_id)
+
+    return render(request, "app/checkout.html", {'product': product})
